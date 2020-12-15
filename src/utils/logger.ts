c83f27c0a5c8f77ce2866f97de9aeb17555df990
pierreyoda <pierreyoda@users.noreply.github.com>
2020-12-15 07:53:13 +0100

    :lipstick: Adjust logger terminal info color for visibility

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,7 +12,7 @@ export interface Logger {
 export const instantiateLogger = (label: string): Logger => {
   const debug = debugLib(`advent-2020-ts:${label}`);
   return {
-    info: (text: string) => debug(chalk.blue(text)),
+    info: (text: string) => debug(chalk.blueBright(text)),
     error: (text: string) => debug(chalk.red(text)),
     warn: (text: string) => debug(chalk.yellow(text)),
     success: (text: string) => debug(chalk.green(text)),
